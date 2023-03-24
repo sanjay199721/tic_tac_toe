e1617b9f4e890507c4165b2e132994dfc2f3219b
sanjay199721 <35732366+sanjay199721@users.noreply.github.com>
2023-03-24 10:50:53 +0530

    changes

--- start.py
+++ start.py
@@ -2,8 +2,9 @@ import pygame as pg
 import random
 import os
 from enum import Enum
+import numpy as np
 
-SCREENRECT = pg.Rect(0,0,640,480)
+
 red_color = (200,20,30)
 black_color = (20,20,20)
 color_light = (170,170,170)
@@ -17,10 +18,11 @@ class game_status(Enum):
 
 pg.init()
 
-
+SCREENRECT = pg.Rect(0,0,640,480)
 font_1 = pg.font.SysFont('Corbel',50)
 font_2 = pg.font.SysFont('Arial',20)
 font_3 = pg.font.Font("BaiJamjuree-Bold.ttf",50)
+font_4 = pg.font.Font("BaiJamjuree-Bold.ttf",30)
 
 clock = pg.time.Clock()
 all = pg.sprite.RenderUpdates()
@@ -28,21 +30,46 @@ all = pg.sprite.RenderUpdates()
 class GlobalState:
     GAME_STATE = game_status.MAIN_MENU
     SCREEN = None
+    def __init__(Self):
+        GlobalState.SCREEN = pg.display.set_mode(SCREENRECT.size)
+GlobalState()
+
+class Main_Menu:
     def __init__(self):
-        screen = pg.display.set_mode(SCREENRECT.size)
         background = pg.Surface(SCREENRECT.size)
         background.fill(red_color)
-        screen.blit(background,(0,0))
         title = font_3.render("Tic-Tac-Toe",True,black_color)
         title_rect = title.get_rect(center = (SCREENRECT.width//2,100))
         background.blit(title,title_rect)
-        
-        screen.blit(background,(0,0))
+        press_ = font_4.render("Play",True,black_color)
+        press_rect1 = press_.get_rect(center = (SCREENRECT.width//2,400))
+        background.blit(press_,press_rect1)
+        Main_Menu.press_rect = press_rect1.inflate(20,15)
+        pg.draw.rect(background, "black",Main_Menu.press_rect , 5, border_radius=10)
+        GlobalState.SCREEN.blit(background,(0,0))
         pg.display.flip()
         smileyball.containers = all
-        smileyball()
-        GlobalState.BGD = background
-        GlobalState.SCREEN = screen
+        self.smiley = smileyball()
+        Main_Menu.BGD = background
+    def update(self):
+
+        clock.tick(40)
+        all.clear(GlobalState.SCREEN,Main_Menu.BGD)
+        self.smiley.update()
+        
+        dirty = all.draw(GlobalState.SCREEN)
+        pg.display.update(dirty)
+        events = pg.event.get()
+        mouse = pg.mouse.get_pos()
+        for event in events:
+            if event.type == pg.QUIT:
+                GlobalState.GAME_STATE = game_status.GAME_END
+            elif event.type == pg.MOUSEBUTTONDOWN:
+                if Main_Menu.press_rect.collidepoint(mouse[0],mouse[1]):
+                    GlobalState.GAME_STATE = game_status.GAME_PLAY
+                    self.smiley.kill()
+
+
 
 main_dir = os.path.split(os.path.abspath(__file__))[0]
 
@@ -76,31 +103,74 @@ class smileyball(pg.sprite.Sprite):
             self.right_facing = self.right_facing*-1
         if self.rect.top<SCREENRECT.top or self.rect.bottom>SCREENRECT.bottom:
             self.top_facing = self.top_facing*-1
-        
-def update_main_menu():
 
-    clock.tick(40)
-    all.clear(GlobalState.SCREEN,GlobalState.BGD)
-    all.update()
-    
-    dirty = all.draw(GlobalState.SCREEN)
-    pg.display.update(dirty)
-    events = pg.event.get()
-    for event in events:
-        if event.type == pg.QUIT:
-            GlobalState.GAME_STATE = game_status.GAME_END
-        elif event.type == pg.KEYDOWN:
-            GlobalState.GAME_STATE = game_status.GAME_END
-    return 
+class Play(pg.sprite.Sprite):
+    image = load_image("hash-removebg-preview.png")
+    image_small = pg.transform.scale(image,(640,480))
+
+
+    def __init__(self):
+        pg.sprite.Sprite.__init__(self,self.containers)
+        background = pg.Surface(SCREENRECT.size)
+        background.fill("blue")
+        GlobalState.SCREEN.blit(background,(0,0))
+        pg.display.update()
+        self.image = self.image_small
+        self.rect = self.image.get_rect(midbottom = SCREENRECT.midbottom)
+        dirty = all.draw(GlobalState.SCREEN)
+        pg.display.update(dirty)
+        #pg.display.update()
+
+        #pg.time.wait(1000)
+    def update(self):
+        clock.tick(40)
+        #dirty = all.draw(GlobalState.SCREEN)
+        #pg.display.update(dirty)
+        events = pg.event.get()
+        #mouse = pg.mouse.get_pos()
+        for event in events:
+            if event.type == pg.QUIT:
+                GlobalState.GAME_STATE = game_status.GAME_END
+            #elif event.type == pg.MOUSEBUTTONDOWN:
+            #    if Main_Menu.press_rect.collidepoint(mouse[0],mouse[1]):
+            #        GlobalState.GAME_STATE = game_status.GAME_PLAY
+
+
+
+def check_game(array):
+    ar = np.append(np.sum(array,1),np.sum(array,0))
+    ar = np.append(ar,[sum(np.diag(y)),sum(np.diag(np.fliplr(y)))])
+    if 3 in ar:
+        return 1
+    elif -3 in ar:
+        return -1
+    else:
+        return 0
+            
+
 
 def main():
 
     logo = load_image("logo32x32.png")
     pg.display.set_icon(logo)
     pg.display.set_caption("Tic Tac Toe")
-    GlobalState()
+    mm = Main_Menu()
     while GlobalState.GAME_STATE == game_status.MAIN_MENU:
-        update_main_menu()
+        mm.update()
+    if GlobalState.GAME_STATE == game_status.GAME_PLAY:
+        Play.containers = all
+        Play()
+        x = np.array([[0,0,0],[0,0,0],[0,0,0]])
+        while GlobalState.GAME_STATE == game_status.GAME_PLAY:
+            events = pg.event.get()
+            for event in events:
+                if event.type == pg.QUIT:
+                    GlobalState.GAME_STATE = game_status.GAME_END
+
+    #clock.tick(1)
+
+    #while GlobalState.GAME_STATE == game_status.GAME_PLAY:
+
     pg.quit()
 if __name__=="__main__":
     main()
